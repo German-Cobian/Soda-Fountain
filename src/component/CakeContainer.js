b4eee0aa05b1e94feb06da6b26ee92b966d2cb00
German Renato Cobian <germancobian@hotmail.com>
2022-01-14 12:17:57 -0800

    Implement single component react-redux app (for buying cakes)

---
+++ src/component/CakeContainer.js
@@ -0,0 +1,15 @@
+import { useSelector, useDispatch } from 'react-redux'
+import { buyCake } from '../redux'
+
+function CakeContainer(props) {
+  const numOfCakes = useSelector(state => state.numOfCakes)
+  const dispatch = useDispatch()
+  return (
+    <div>
+      <h2>Number of cakes - {numOfCakes}</h2>
+      <button onClick={() => dispatch(buyCake())}>Buy Cake</button>
+    </div>
+  )
+}
+
+export default CakeContainer
