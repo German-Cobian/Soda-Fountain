1fc1843e5fda6d35be164e1bc1ddd15171a46415
German Renato Cobian <germancobian@hotmail.com>
2022-01-14 14:14:25 -0800

    Implement redux devtools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,12 @@
-import { createStore, applyMiddleware } from 'redux' // "applyMiddleware" for use of logger
-import logger from 'redux-logger' // for logger middleware
+import { createStore, applyMiddleware } from 'redux'
+import logger from 'redux-logger'
 import rootReducer from './rootReducer'
+import { composeWithDevTools } from 'redux-devtools-extension'; // for devtools
 
-const store = createStore(rootReducer, applyMiddleware(logger)) // "applyMiddleware(logger)" added
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger))) 
+// "composeWithDevTools() added for devtools usage"
 
 export default store
+
+// Documentation for devtools at https://github.com/zalmoxisus/redux-devtools-extension
+// Got to Usage section 1.3 "1.3 Use redux-devtools-extension package from npm" for instructions
